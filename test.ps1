9145d851cba32656d0cc35166caadbef9b47a002
wangzq <wangzq@gmail.com>
2017-02-03 20:31:13 -0800

    freemind, sqliteadmin, sqliteman

--- test.ps1
+++ test.ps1
@@ -1,16 +1,18 @@
 [CmdletBinding()]
 param (
-	[Parameter(Mandatory=$true)]
 	[string] $Name
 	)
+if (!$Name) {
+    dir "$PsScriptRoot\*.json" | % { [IO.Path]::GetFileNameWithoutExtension($_) }
+} else {
+    $preinstall = "$PsScriptRoot\$Name.pre_install.ps1"
+    if (Test-Path $preinstall) {
+        & "$PsScriptRoot\inject-script.ps1" "$PsScriptRoot\$Name.json" @(
+            "$PsScriptRoot\common.ps1"
+            $preinstall
+        )
+    }
 
-$preinstall = "$PsScriptRoot\$Name.pre_install.ps1"
-if (Test-Path $preinstall) {
-    & "$PsScriptRoot\inject-script.ps1" "$PsScriptRoot\$Name.json" @(
-        "$PsScriptRoot\common.ps1"
-        $preinstall
-    )
+    scoop uninstall $Name
+    scoop install "$PsScriptRoot\$Name.json"
 }
-
-scoop uninstall $Name
-scoop install "$PsScriptRoot\$Name.json"
